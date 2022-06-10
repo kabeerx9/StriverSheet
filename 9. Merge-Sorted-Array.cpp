612212b4b1a5828401f2c87cd316cb08acf7599d
kabeerx9 <89746908+kabeerx9@users.noreply.github.com>
2022-06-10 12:33:03 +0530

    Create 9. Merge-Sorted-Array.cpp

---
+++ 9. Merge-Sorted-Array.cpp
@@ -0,0 +1,18 @@
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j =n-1;
+        int tar = n+m-1;
+        
+        while (j >=0){
+            if (i>=0 && nums1[i] > nums2[j]){
+                nums1[tar] = nums1[i];
+                tar--;
+                i--;
+            }
+            else{
+                nums1[tar] = nums2[j];
+                tar--;
+                j--;
+            }
+        }
+    }
