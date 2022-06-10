6528a377d9863296006e435fd09b5a8a1aeee56d
kabeerx9 <89746908+kabeerx9@users.noreply.github.com>
2022-06-10 12:27:41 +0530

    Create 4. Kadane'sAlgo.cpp

---
+++ 4. Kadane'sAlgo.cpp
@@ -0,0 +1,26 @@
+    // msf = maximum so far 
+    // meh = maximum end here 
+    
+    int maxSubArray(vector<int>& nums) {
+        
+        int msf = nums[0];
+        int meh = 0;
+        
+        for(int i=0 ; i<nums.size() ; i++)
+        {
+            meh = meh + nums[i];
+            
+            if(msf< meh)
+            {
+                msf = meh;
+            }
+            
+            if(meh < 0)
+            {
+                meh = 0;
+            }
+        }
+        
+        return msf;
+        
+    }
